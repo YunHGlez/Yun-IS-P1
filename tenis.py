b8fa001ae457444901a14fdc7e3e081fd6c2ac0e
Yun Hernandez Gonzalez <72617911+YunHGlez@users.noreply.github.com>
2023-08-30 22:47:01 -0600

    version funcional
    funciona el marcador:
    -necesita comentarios y limpieza
    -posible boton para imprimir marcador

--- tenis.py
+++ tenis.py
@@ -7,28 +7,26 @@
 - 6 juegos hacen un set
 
 """
-
 class Marcador:
     j1 = ""
     j2 = ""
     pj1 = 0
     pj2 = 0
-    setCount = 0
     setj1 = 0
     setj2 = 0
+    juegosj1 = 0
+    juegosj2 = 0
+    cambioCancha = 0
     arrPuntos = ["0", "15", "30", "40", "ad","gana juego"]
-    arr = [[0, 0, 0],[0, 0, 0]]
+    arr = [1,3,5,7]
     """
     
     """
     def __str__(self):
         buff1 = self.pj1
         buff2 = self.pj2
-        st = f"MARCADOR \n{self.j1} puntos: {self.arrPuntos[buff1]} r: {self.pj1}\n"
-        st += f"{self.j2} puntos: {self.arrPuntos[buff2]} r: {self.pj2}\n"
-        st += f"set j1 {self.setj1} | set j2 {self.setj2}"
-        #st += f"J1 Set 1:  {self.set1j1} Set 2:  {self.set2j1} Set 3:  {self.set3j1} \n"
-        #st += f"J2 Set 1:  {self.set1j2} Set 2:  {self.set2j2} Set 3:  {self.set3j2} \n"
+        st = f"MARCADOR \n{self.j1} puntos: {self.arrPuntos[buff1]} | juegos ganados {self.juegosj1} | sets ganados {self.setj1}\n"
+        st += f"{self.j2} puntos: {self.arrPuntos[buff2]}  | juegos ganados {self.juegosj2} | sets ganados {self.setj2}\n"
         return st
     """
     {self.arrPuntos[buff2]}
@@ -40,69 +38,80 @@ class Marcador:
         self.j2 = j2
 
     def ganaJuego(self, a, b):
+        
         if a == 4 and (b == 2 or b == 1 or b == 0) :
-            self.setj1 =+ 1
-            self.ganaJuego(self.setj1, self.setj2)
+            self.juegosj1 += 1
             self.reiniciaPuntos()
         elif (a == 2 or a == 1 or a == 0) and b == 4:
-            self.setj2 =+ 1
-            self.ganaJuego(self.setj1, self.setj2)
+            self.juegosj2 += 1
             self.reiniciaPuntos()
         elif a == 4 and b == 6:
-            self.setj1 =+ 1
-            self.ganaJuego(self.setj1, self.setj2)
+            self.juegosj1 += 1
             self.reiniciaPuntos()
         elif a == 6 and b == 4:
-            self.setj2 =+ 1
-            self.ganaJuego(self.setj1, self.setj2)
+            self.juegosj2 += 1
             self.reiniciaPuntos()
         # D E U C E
         elif a == 4 and b == 4:
             self.pj1 -= 1
             self.pj2 -= 1
         elif a == 3 and b == 5:
-            self.setj2 =+ 1
-            self.ganaJuego(self.setj1, self.setj2)
+            self.juegosj2 += 1
             self.reiniciaPuntos()
         elif a == 5 and b == 3:
-            self.setj1 =+ 1
-            self.ganaJuego(self.setj1, self.setj2)
+            self.juegosj1 += 1
             self.reiniciaPuntos()
 
     def ganaSet(self, a, b):
-        if a == 5 and (b == 3 or b == 2 or b == 1 or b == 0) :
-            self.set1j1 =+ 1
-        elif b == 5 and (a == 3 or a == 2 or a == 1 or a == 0):
-            self.set1j2 =+ 1
+
+        if a == 6 and (b ==4 or b == 3 or b == 2 or b == 1 or b == 0) :
+            self.setj1 += 1
+            self.reiniciaPuntosSet()
+        elif b == 6 and (b ==4 or a == 3 or a == 2 or a == 1 or a == 0):
+            self.setj2 += 1
             self.reiniciaPuntosSet()
         elif a == 5 and b == 7:
-            self.set1j1 =+ 1
+            self.setj1 += 1
             self.reiniciaPuntosSet()
         elif a == 7 and b == 5:
-            self.set1j2 =+ 1
+            self.setj1 += 1
             self.reiniciaPuntosSet()
         # P U N T O DE V E N T A J A
         elif a == 6 and b == 6:
-            self.pj1 -= 1
-            self.pj2 -= 1
+            self.juegosj1 -= 1
+            self.juegosj2 -= 1
+            print("punto de ventaja")
         elif a == 4 and b == 6:
-            self.set1j2 =+ 1
+            self.setj2 += 1
             self.reiniciaPuntosSet()
         elif a == 6 and b == 4:
-            self.set1j1 =+ 1
+            self.setj1 += 1
             self.reiniciaPuntosSet()
+
+        #Caso para comprobar si alguno gano el juego
         if self.setj1 == 2:
-            print(f"{self.j1} ha ganado el partido!")
-        if self.setj2 == 2:
-            print(f"{self.j2} ha ganado el partido!")
+            print(f"***********{self.j1} ha ganado el partido!***********")
+            exit()
+
+        elif self.setj2 == 2:
+            print(f"***********{self.j2} ha ganado el partido!***********")
+            exit()
     
-    def impGanaJuego(self, a):
-        print(f'{a} gana el juego!')
+    def cambioDeCancha(self):
+        if self.cambioCancha in self.arr:
+            print("\n----Cambio de cancha----\n")
 
     def reiniciaPuntos(self):
         self.pj1 = 0
         self.pj2 = 0
-
+        self.cambioCancha += 1
+        self.cambioDeCancha()
+        self.ganaSet(self.juegosj1, self.juegosj2)
+    
+    def reiniciaPuntosSet(self):
+        self.juegosj1 = 0
+        self.juegosj2 = 0
+        self.cambioCancha = 0
 
     def aPuntoj1(self):
         self.pj1 += 1
